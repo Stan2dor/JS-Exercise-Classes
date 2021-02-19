6c178cd693f29bff5e3cc5a530ad2a8a58188b76
Stan Tudor <admin@Mac.local>
2021-02-18 20:20:20 -0800

    Task-3 & 4

--- index.js
+++ index.js
@@ -41,23 +41,24 @@ class Airplane {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.stomach = [];
-}
-Person.prototype.eat = function (someFood) {
-  if (this.stomach.length < 10) {
-    this.stomach.push(someFood);
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
-};
-Person.prototype.poop = function () {
-  this.stomach = [];
-};
-Person.prototype.toString = function () {
-  return `${this.name}, ${this.age}`;
-};
-
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return (`${this.name}, ${this.age}`);
+  }
+}
 let me = new Person("Stan", 46);
 console.log(me.toString());
 
@@ -76,7 +77,21 @@ console.log(me.toString());
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    this.odometer += distance;
+    this.tank = this.tank - distance;
+    if (this.tank === 0)
+      return (`I run out of fuel at ${this.odometer} miles!`)
+  }
 }
 
 /*
@@ -92,7 +107,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(obj) {
+    this.name = obj.name,
+      this.age = obj.age,
+      this.location = obj.location
+  }
+  speak() {
+    return (`Hello my name is ${this.name}, I am from ${this.location}`)
+  }
 }
 
 /*
@@ -109,8 +131,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(obj) {
+    super(obj);
+    this.specialty = obj.specialty;
+    this.favLanguage = obj.favLanguage;
+    this.catchPhrase = obj.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 /*
   TASK 5
